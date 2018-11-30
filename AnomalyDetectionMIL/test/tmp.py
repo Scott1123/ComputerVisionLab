ed9def76a8e53aa3ec8fca287999c24824969d8e
Scott Yang <noble1123@outlook.com>
2018-11-30 16:51:54 +0800

    #

--- AnomalyDetectionMIL/test/tmp.py
+++ AnomalyDetectionMIL/test/tmp.py
@@ -1,17 +1,24 @@
 # !/usr/bin/python
 # -*-coding:utf-8-*-
-import numpy as np
-import keras.backend as K
-import tensorflow as tf
+# import numpy as np
+# import keras.backend as K
+# import tensorflow as tf
 
+#
+# x = np.array([1, 2, 5, 5, 4, 3, 5, 5, 10, 9])
+# y = np.array([18, 28, 58, 582, 48, 38, 58, 58, 108, 98])
+#
+# a = tf.greater_equal(x, 5)
+#
+# b = tf.boolean_mask(y, a)
+#
+# print(b)
+# print(K.eval(b))
 
-x = np.array([1, 2, 5, 5, 4, 3, 5, 5, 10, 9])
-y = np.array([18, 28, 58, 582, 48, 38, 58, 58, 108, 98])
-print(x)
-res = tf.Variable(x)
-ad = K.sum(3)
-print(ad)
-res = K.concatenate([res, [ad]])
+array = [1, 8, 15]
+g = (x for x in array if array.count(x) > 0)
+# array = [2, 8, 22]
 
-print(res)
-print(K.eval(res))
+print(type(g))
+
+print(list(g))
