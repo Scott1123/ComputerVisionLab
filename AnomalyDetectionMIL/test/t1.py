aa7b6bbc8d494a860860dce7f52ad4d3b80edc6e
Scott Yang <noble1123@outlook.com>
2018-12-20 09:49:06 +0800

    #

--- AnomalyDetectionMIL/test/t1.py
+++ AnomalyDetectionMIL/test/t1.py
@@ -3,15 +3,8 @@ import numpy as np
 
 TRAIN_DATA_DIR = 'E:/github_projects/data/UCF_Anomaly_Dataset/C3D_Features/Train/'
 
-a = np.random.randint(1, 100, (5, 10))
+a = np.random.randint(1, 100, (32, 1))
+a = a.tolist()
+b = sum(a, [])
 
-a = a / 1000
-print(a)
-
-with open(TRAIN_DATA_DIR + '1.txt', 'w') as f:
-    for i in range(5):
-        for j in range(10):
-            f.write('%.4f ' % (a[i][j]))
-        f.write('\n')
-
-print('end.')
+print(b)
