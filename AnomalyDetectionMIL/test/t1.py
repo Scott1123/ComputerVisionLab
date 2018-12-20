3975e72105570d23056c68f289b86b1fe821df84
Scott Yang <noble1123@outlook.com>
2018-12-20 22:40:39 +0800

    # test gpu

--- AnomalyDetectionMIL/test/t1.py
+++ AnomalyDetectionMIL/test/t1.py
@@ -1,10 +1,16 @@
-import numpy as np
+# import numpy as np
+#
+#
+# TRAIN_DATA_DIR = 'E:/github_projects/data/UCF_Anomaly_Dataset/C3D_Features/Train/'
+#
+# a = np.random.randint(1, 100, (32, 1))
+# a = a.tolist()
+# b = sum(a, [])
+#
+# print(b)
 
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
 
-TRAIN_DATA_DIR = 'E:/github_projects/data/UCF_Anomaly_Dataset/C3D_Features/Train/'
-
-a = np.random.randint(1, 100, (32, 1))
-a = a.tolist()
-b = sum(a, [])
-
-print(b)
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
